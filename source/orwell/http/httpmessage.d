9907b5e72581fe64d88d15db1a47802a565a5345
Matthew Remmel <matt.remmel@gmail.com>
2017-08-09 01:31:46 -0400

    Added module with generic structs and enums for http messages

---
+++ source/orwell/http/httpmessage.d
@@ -0,0 +1,59 @@
+module orwell.http.httpmessage;
+
+public import etc.enums;
+
+/**
+  * HTTP Schemes
+  */
+enum HttpScheme {
+    HTTP = "http",
+    HTTPS = "https",
+    FTP = "ftp"
+}
+
+/**
+  * HTTP/1.1 Methods
+  */
+enum HttpMethod {
+    GET =         "GET",  /// Requests a representation of the specified resource.
+    HEAD =       "HEAD",  /// Requests a response identicial to that of GET, but without the response body.
+    POST =       "POST",  /// Used to submit an entity to the specified resource.
+    PUT =         "PUT",  /// Replaces all current representations of the target resource with the request payload.
+    DELETE =   "DELETE",  /// Requests a delete of the specified resource.
+    CONNECT = "CONNECT",  /// Used to establish a tunnel to the server identified by the target resource.
+    OPTIONS = "OPTIONS",  /// Used to request the communications options for the target resource.
+    TRACE =     "TRACE",  /// Performs a message loop-back test along the path to the target resource.
+    PATCH =     "PATCH"   /// Used to apply partial modifications to a resource.
+}
+
+/**
+  * HTTP Versions
+  */
+enum HttpVersion {
+    v1 = "HTTP/1",
+    v1_1 = "HTTP/1.1",
+    v2 = "HTTP/2"
+}
+
+/**
+  * HTTP Status Code
+  */
+alias HttpStatusCode = int;
+
+/**
+  * HTTP Status Text
+  */
+alias HttpStatusText = string;
+
+/**
+  * HTTP Header
+  */
+struct HttpHeader {
+    string key;     /// The header key
+    string value;   /// The header value
+
+    this(string key, string value) {
+        this.key = key;
+        this.value = value;
+    }
+}
